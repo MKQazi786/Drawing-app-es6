f9e637abd2685030917e478411ee003098fa4e19
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2024-10-22 00:28:51 +0500

    I have added many code and connect with directly github to vs code

--- index.js
+++ index.js
@@ -12,11 +12,34 @@ let prevY = null;
 
 let draw = false;
 
-body.style.backgroundColor ="#ffffff";
+body.style.backgroundColor = "#ffffff";
 
 let theInput = document.getElementById("favcolor");
 
-theInput.addEventListener("input",()=>{
+theInput.addEventListener("input", () => {
     color = theInput.value;
     body.style.backgroundColor = color;
 });
+
+let ctx = canvas.getContext("2d");
+ctx.lineWidth = lineW;
+
+document.getElementById("ageInputId").oninput = () => {
+    draw = null;
+    lineW = document.getElementById("ageInputId").value;
+    document.getElementById("ageOutputId").innerHTML = lineW;
+    ctx.lineWidth = lineW;
+};
+
+let clrs = document.querySelectorAll(".clr")
+clrs.map((clr)=>{
+    clr.addEventListener("click",()=>{
+        ctx.strokeStyle = clr.dataset.clr;
+    })
+})
+
+let clearBtn = document.querySelectorAll(".clear");
+
+clearBtn.addEventListener("click",()=>{
+    location.reload();
+})
